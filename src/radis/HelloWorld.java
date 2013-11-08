133584870de535f5e78c8ead31409ff343e074ca
Ali Imran <aliimranawan@hotmail.com>
2013-11-08 10:52:25 +0100

    Maybe Bjørn is Gay

--- src/radis/HelloWorld.java
+++ src/radis/HelloWorld.java
@@ -5,10 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Bj��rn er en slave.");
 		System.out.println("Bjørn er sød");
+		
 	}
 	
 	public void hello(){
 		System.out.println("Bj�rn is gay?");
+		System.out.println("I Agree with that !!!");
 	}
 }
 
