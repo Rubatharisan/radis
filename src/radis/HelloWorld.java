d5ee1e5401afec25de05062823b031723a438bee
rubas <rubas@10.126.8.222>
2013-11-08 15:51:27 +0100

    Removed Bjorn

--- src/radis/HelloWorld.java
+++ src/radis/HelloWorld.java
@@ -3,18 +3,7 @@ package radis;
 public class HelloWorld {
 
 	public static void main(String[] args){
-		System.out.println("Bj��rn er en slave.");
-		System.out.println("Bjørn er sød");
-
-
-		System.out.println("I lied, Bjørn is gay");
-
-		System.out.println("I lied, Bjørn is gay");
-	
-
-	
-		System.out.println("Andreas is gay?");
-		System.out.println("I Agree with that !!!");
+		
 	}
 }
 
