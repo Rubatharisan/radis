ba360630e381f7bf4dbd25e20c661fad233894c5
Andreas Kastrup <akast11@student.sdu.dk>
2013-11-08 10:52:12 +0100

    jeg skrev jeg løj, tihi.

--- src/radis/HelloWorld.java
+++ src/radis/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Bj��rn er en slave.");
 		System.out.println("Bjørn er sød");
+		System.out.println("I lied, Bjørn is gay");
 	}
 	
 	public void hello(){
