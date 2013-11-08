e7c9e52211c977d7fe925ad1e5bcd65fc1f16608
Ali Imran <aliimranawan@hotmail.com>
2013-11-08 10:56:44 +0100

    Rettelser

--- src/radis/HelloWorld.java
+++ src/radis/HelloWorld.java
@@ -5,14 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Bj��rn er en slave.");
 		System.out.println("Bjørn er sød");
-<<<<<<< HEAD
-		
-=======
+
 		System.out.println("I lied, Bjørn is gay");
->>>>>>> ba360630e381f7bf4dbd25e20c661fad233894c5
-	}
 	
-	public void hello(){
 		System.out.println("Bj�rn is gay?");
 		System.out.println("I Agree with that !!!");
 	}
